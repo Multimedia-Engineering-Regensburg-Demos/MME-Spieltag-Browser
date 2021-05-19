fb28d1e49c42b10f3a2252d0b06950a57222c3bd
Alexander Bazo <alexanderbazo@googlemail.com>
2021-05-19 08:10:07 +0200

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,8 @@
+# Der Spieltag-Browser
+
+Informationen und eine Anleitung zur Implementierung der Demo finden Sie [hier](https://regensburger-forscher.de/mme/Demos/spieltag-browser/).
+
+## Download
+
+- [Starter-Paket (zip-Datei)](https://github.com/Multimedia-Engineering-Regensburg-Demos/MME-Spieltag-Browser/archive/starter.zip)
+- [Lösungsvorschlag (zip-Datei)](https://github.com/Multimedia-Engineering-Regensburg-Demos/MME-Spieltag-Browser/archive/master.zip)
---
+++ resources/js/index.js
@@ -0,0 +1,21 @@
+/* eslint-env browser */
+
+/**
+ * Hinweise zur Nutzung der openligadb-API
+ * 
+ * Zugriff auf einzelne Spieltage der Bundesliga
+ * URL: https://www.openligadb.de/api/getmatchdata/bl1/SAISON/SPIELTAG
+ * Beispiel: https://www.openligadb.de/api/getmatchdata/bl1/2020/34
+ * 
+ * Zugriff auf alle Spiele der Bundesliga
+ * URL: https://www.openligadb.de/api/getmatchdata/bl1/SAISON
+ * Beispiel: https://www.openligadb.de/api/getmatchdata/bl1/2020
+ * 
+ * Achtung: Die Daten werden von einem Community-Projekt gepflegt und kostenfrei bereitgestellt. Das
+ * sollten wir bei der Nutzung der API berücksichtigen und z.B. möglichst sparsam mit den Anfragen
+ * an den Server umgehen. Gerade bei Daten, die sich nur innerhalb bestimmter Zeiträume ändern 
+ * sollten, unabhängig vom Antwortverhalten des Servers, lokale Caches verwendet werden. Die hier
+ * verwendeten Daten, werden sich bis zum nächsten (letzten) Spieltag nicht ändern.
+ */
+
+console.log("Starting Spieltag-Browser");
